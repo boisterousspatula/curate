e1887bba43862ddf3b949f5ee2791453bc13f881
Anuj Tomar <ankuto@gmail.com>
2015-05-27 16:39:13 -0700

    cleaned up code, created clone object function

--- client/scripts/stores/sections.js
+++ client/scripts/stores/sections.js
@@ -9,13 +9,16 @@ var sectionDefaults = require('../constants/defaults');
 var CHANGE_EVENT = 'change';
 var _sections = [];
 
+var cloneObj = function(obj){
+  //returns a new copy of the object, used whenever we need to push a copy of a data structure into _sections
+  return JSON.parse(JSON.stringify(obj));
+}
 
 var SectionStore = new Store({
-  //starts the _sections repository with a section
-
+  //starts the _sections repository with a section and a link
   init: function(){
-    var newSection = JSON.parse(JSON.stringify(sectionDefaults.section));
-    var newLink = JSON.parse(JSON.stringify(sectionDefaults.link));
+    var newSection = cloneObj(sectionDefaults.section);
+    var newLink = cloneObj(sectionDefaults.link);
     _sections.push(newSection)
     _sections[0].links.push(newLink)
 
@@ -33,7 +36,6 @@ var SectionStore = new Store({
 	get: function() {
 		return _sections;
 	}
-
   
 });
 
@@ -47,23 +49,20 @@ SectionStore.dispatcherToken = Dispatcher.register(function(payload) {
 		SectionStore.emitChange();
 	}
   else if(action.actionType === sectionConstants.CREATE_NEW_SECTION){
-    //pushes a new section template to object
     console.log("in section store, pushing section")
 
-    var newSection = JSON.parse(JSON.stringify(sectionDefaults.section));
-
+    //adds a new section to _sections
+    var newSection = cloneObj(sectionDefaults.section);
     _sections.push(newSection);
     SectionStore.emitChange();
   }
   else if(action.actionType === sectionConstants.CREATE_NEW_LINK){
+    console.log("in section store, pushing link at index");
+    //adds a new link to _sections at the index of the section that the add button was clicked
     var index = payload.action.index
-
-    console.log("in section store, pushing link at index", index);
-
-    var newLink = JSON.parse(JSON.stringify(sectionDefaults.link));
+    var newLink = cloneObj(sectionDefaults.link);
     _sections[index].links.push(newLink);
     SectionStore.emitChange();
-
   }
 
 });
