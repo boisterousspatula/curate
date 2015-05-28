3038bb8589012f569f01bdfa50aea29e7804d093
davidhopper <hopperman@gmail.com>
2015-05-27 20:03:08 -0700

    (feat) Update relationships for guide and section tables

--- server/models/section.js
+++ server/models/section.js
@@ -14,6 +14,7 @@ var SectionModel = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         Section.hasMany(models.link);
+        Section.hasOne(models.guide);
       }
     }
   });
