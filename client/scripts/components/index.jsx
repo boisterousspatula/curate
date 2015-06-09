6e8079c2bca51721523fa48eb8f0c650ddb61837
davidhopper <hopperman@gmail.com>
2015-06-09 15:26:57 -0700

    (feat) Update styling to try and achieve mockup design

--- client/scripts/components/index.jsx
+++ client/scripts/components/index.jsx
@@ -14,13 +14,22 @@ var IndexComponent = React.createClass({
       /* jshint ignore:start */
       <DefaultLayout>
         <div className="main-container">
-          <div className="yeogurt-info">
-            <h1>≈skill.it!</h1>
-            <GuideList/>
+          <div className="homepage">
+          
+            <section className="cover">
+              <h1 className="title">Crowd-sourced Curriculums</h1>
+              <h2 className="description">Sign up and create your own learning path. Share it with the world 
+              and help others find the most effective material on the internet.</h2>
+            </section>
+
+            <section className="guide-list">
+              <GuideList/>
+            </section>
+
           </div>
-          <button className="guide-create" onClick={this.handleNewGuide}>Create New Guide</button>
-					<a className="btn-floating btn-large waves-effect waves-light red"><i className="mdi-content-add"></i></a>
-        </div>
+            <button className="guide-create" onClick={this.handleNewGuide}>Create New Guide</button>
+					  <a className="btn-floating btn-large waves-effect waves-light red"><i className="mdi-content-add"></i></a>
+          </div>
       </DefaultLayout>
       /* jshint ignore:end */
     );
