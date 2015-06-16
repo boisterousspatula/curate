9d94f9b659a4035e08555539bdedca0828e11c9e
Anuj Tomar <ankuto@gmail.com>
2015-06-15 22:36:30 -0700

    cleaned up indents

--- client/scripts/components/index.jsx
+++ client/scripts/components/index.jsx
@@ -34,61 +34,55 @@ var IndexComponent = React.createClass({
 
             <section className="about-us">
               <div className="crosshatch containers">
-
-                  <h1 className="aboutTitle">Meet the Team</h1>
-                  <div className="people">
-                    <div className="john">
-                      <div className="name">John Yeglinski</div>
-                        <div className="social">
-                          <a href="https://github.com/jyeg">
-                            <div className="git"></div>
-                          </a>
-                          <a href="https://www.linkedin.com/in/johnyeg">
-                            <div className="linkd"></div>
-                          </a>
-                        </div>
-                    </div>
-
-                    <div className="anuj">
-                     <div className="name">Anuj Tomar</div>
+                <h1 className="aboutTitle">Meet the Team</h1>
+                <div className="people">
+                  <div className="john">
+                    <div className="name">John Yeglinski</div>
                       <div className="social">
-                        <a href="https://github.com/tomarak">
+                        <a href="https://github.com/jyeg">
                           <div className="git"></div>
                         </a>
-                        <a href="https://www.linkedin.com/in/tomarak">
+                        <a href="https://www.linkedin.com/in/johnyeg">
                           <div className="linkd"></div>
                         </a>
                       </div>
-                    </div>
-
-                    <div className="vince">
-                      <div className="name">Vince Tam</div>
-                        <div className="social">
-                        <a href="https://github.com/waterdoo">
-                          <div className="git"></div>
-                        </a>
-                        <a href="https://www.linkedin.com/in/vincetam">
-                          <div className="linkd"></div>
-                        </a>
-                      </div>
-                    </div>
-
-                    <div className="david">
-                      <div className="name">David Hopper</div>
-                        <div className="social">
-                          <a href="https://github.com/davidhopper">
-                            <div className="git"></div>
-                            </a>
-                          <a href="https://www.linkedin.com/in/dphopper">
-                            <div className="linkd"></div>
-                          </a>
-                        </div>
-                    </div>
-
                   </div>
+                  <div className="anuj">
+                   <div className="name">Anuj Tomar</div>
+                    <div className="social">
+                      <a href="https://github.com/tomarak">
+                        <div className="git"></div>
+                      </a>
+                      <a href="https://www.linkedin.com/in/tomarak">
+                        <div className="linkd"></div>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="vince">
+                    <div className="name">Vince Tam</div>
+                      <div className="social">
+                      <a href="https://github.com/waterdoo">
+                        <div className="git"></div>
+                      </a>
+                      <a href="https://www.linkedin.com/in/vincetam">
+                        <div className="linkd"></div>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="david">
+                    <div className="name">David Hopper</div>
+                      <div className="social">
+                        <a href="https://github.com/davidhopper">
+                          <div className="git"></div>
+                          </a>
+                        <a href="https://www.linkedin.com/in/dphopper">
+                          <div className="linkd"></div>
+                        </a>
+                      </div>
+                  </div>
+                </div>
               </div>
             </section>
-
           </div>
         </div>
       </DefaultLayout>
